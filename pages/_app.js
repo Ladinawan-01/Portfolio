67663198369b93b17d2f896fca42f51538c381a0
ladinawan4 <ladin@webile.io>
2024-10-04 16:10:05 +0500

    Vercel Speed

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import '../styles/globals.css';
- 
+
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />
+            <SpeedInsights />
+        </>
+    );
 }
